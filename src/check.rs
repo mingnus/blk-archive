408cfe4c816c1d2672836b37334824668126eb0c
Ming-Hung Tsai <mtsai@redhat.com>
2025-04-15 10:34:39 +0800

    [check] Multithreaded check
    The SlabFile cannot be cloned into threads due to there's a
    mpsc::Receiver inside the CompressionService, where the Receiver is not
    Sync.

--- src/check.rs
+++ src/check.rs
@@ -8,18 +8,45 @@ use crate::hash::hash_256_iov;
 use crate::hash_index::ByIndex;
 use crate::output::Output;
 use crate::paths::*;
-use crate::slab::SlabFileBuilder;
+use crate::slab::{SlabFile, SlabFileBuilder};
+
+fn check_data_and_hashes(
+    data_file: Arc<SlabFile>,
+    hashes_file: Arc<SlabFile>,
+    slab_range: std::ops::Range<u32>,
+) -> Result<()> {
+    for s in slab_range {
+        let data = data_file.read_uncached(s)?;
+        let hashes = ByIndex::new(Arc::new(hashes_file.read_uncached(s)?))?;
+
+        for i in 0..hashes.len() {
+            let (data_begin, data_end, expected_hash) = hashes
+                .get(i)
+                .ok_or_else(|| anyhow!("Failed to get hash entry {} at slab {}", i, s))?;
+            let iov = vec![&data[*data_begin as usize..*data_end as usize]];
+            if hash_256_iov(&iov) != *expected_hash {
+                return Err(anyhow!("Unexpected hash at slab {} index {}", s, i));
+            }
+        }
+    }
+
+    Ok(())
+}
 
 fn check(archive_dir: &Path) -> Result<()> {
     env::set_current_dir(&archive_dir)?;
 
-    let mut data_file = SlabFileBuilder::open(data_path())
-        .build()
-        .context("couldn't open data slab file")?;
+    let data_file = Arc::new(
+        SlabFileBuilder::open(data_path())
+            .build()
+            .context("couldn't open data slab file")?,
+    );
 
-    let mut hashes_file = SlabFileBuilder::open(hashes_path())
-        .build()
-        .context("couldn't open hashes slab file")?;
+    let hashes_file = Arc::new(
+        SlabFileBuilder::open(hashes_path())
+            .build()
+            .context("couldn't open hashes slab file")?,
+    );
 
     let nr_data_slabs = data_file.get_nr_slabs();
 
@@ -31,20 +58,17 @@ fn check(archive_dir: &Path) -> Result<()> {
         return Err(anyhow!("Too many slabs"));
     }
 
-    for s in 0..nr_data_slabs as u32 {
-        let data = data_file.read(s)?;
-        let hashes = ByIndex::new(hashes_file.read(s)?)?;
-
-        // TODO: multithreading
-        for i in 0..hashes.len() {
-            let (data_begin, data_end, expected_hash) = hashes
-                .get(i)
-                .ok_or_else(|| anyhow!("Failed to get hash entry {} at slab {}", i, s))?;
-            let iov = vec![&data[*data_begin as usize..*data_end as usize]];
-            if hash_256_iov(&iov) != *expected_hash {
-                return Err(anyhow!("Unexpected hash at slab {} index {}", s, i));
-            }
-        }
+    let nr_threads = 2;
+    let slabs_per_thread = nr_data_slabs / nr_threads;
+    let mut slab_begin = 0;
+    for i in 0..nr_threads - 1 {
+        std::thread::spawn(|| {
+            check_data_and_hashes(
+                data_file.clone(),
+                hashes_file.clone(),
+                slab_begin..slab_begin + slabs_per_thread as u32,
+            )
+        });
     }
 
     Ok(())
